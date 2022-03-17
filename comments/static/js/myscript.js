80dd4475be4aaa16fc3452cfbf4d6b340aa0ef45
Heyzeus85 <jdwalters85@gmail.com>
2022-03-17 16:28:31 -0700

    Update myscript.js

--- comments/static/js/myscript.js
+++ comments/static/js/myscript.js
@@ -1,7 +1,7 @@
 function writeComment(event) {
     event.preventDefault();
 
-    fetch('api/options/').then(response => response.json()).then(data => composeComment(data));
+    fetch('/api/options/').then(response => response.json()).then(data => composeComment(data));
 
     function composeComment(data) {
         let name = document.getElementById('name').value;
